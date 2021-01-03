a4e2036a42d451589f273b550e929c4c75e814d3
Mat Jones <mjones44@ycp.edu>
2021-01-02 19:24:40 -0500

    add statically.io cdn cache

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const isProd = process.env.NODE_ENV === "production";
+
 const withMDX = require("@next/mdx")({
   extension: /\.(md|mdx)$/,
   options: {
@@ -6,4 +8,8 @@ const withMDX = require("@next/mdx")({
   },
 });
 
-module.exports = withMDX();
+module.exports = withMDX({
+  assetPrefix: isProd
+    ? "https://cdn.statically.io/gh/mrjones2014/mrjones2014.github.io/gh-pages/"
+    : "",
+});
