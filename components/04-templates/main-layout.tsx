9b878520bc775a1cdda730d968f0b0bb1431a3f7
Mat Jones <mjones44@ycp.edu>
2021-01-02 19:48:04 -0500

    add page title

--- components/04-templates/main-layout.tsx
+++ components/04-templates/main-layout.tsx
@@ -1,13 +1,9 @@
-import {
-  FocusStyleManager,
-  Navbar,
-  NavbarGroup,
-  NavbarHeading,
-} from "@blueprintjs/core";
+import { FocusStyleManager } from "@blueprintjs/core";
 import { MainNav } from "components/02-molecules/main-nav/main-nav";
 import useSafariVhWorkaround from "hooks/utils/use-safari-vh-workaround";
 import { ChildrenProps } from "interfaces/children-props";
 import React, { useEffect, useState } from "react";
+import Head from "next/head";
 import Styles from "./main-layout.module.scss";
 
 export const MainLayout: React.FC<ChildrenProps> = (props: ChildrenProps) => {
@@ -29,9 +25,14 @@ export const MainLayout: React.FC<ChildrenProps> = (props: ChildrenProps) => {
   }
 
   return (
-    <div className={classNames.join(" ")}>
-      <MainNav useDarkTheme={useDarkTheme} onThemeToggled={toggleDarkTheme} />
-      {props.children}
-    </div>
+    <React.Fragment>
+      <Head>
+        <title>mjones.network</title>
+      </Head>
+      <div className={classNames.join(" ")}>
+        <MainNav useDarkTheme={useDarkTheme} onThemeToggled={toggleDarkTheme} />
+        {props.children}
+      </div>
+    </React.Fragment>
   );
 };
