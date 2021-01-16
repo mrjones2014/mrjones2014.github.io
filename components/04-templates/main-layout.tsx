649f5c98bb95105e51aa73068014e45cd358b554
Mat Jones <mjones44@ycp.edu>
2021-01-16 12:02:56 -0500

    update favicon

--- components/04-templates/main-layout.tsx
+++ components/04-templates/main-layout.tsx
@@ -28,11 +28,22 @@ export const MainLayout: React.FC<ChildrenProps> = (props: ChildrenProps) => {
     <React.Fragment>
       <Head>
         <title>mjones.network</title>
-        <link rel="shortcut icon" href="https://mjones.network/favicon.svg" />
-        <link rel="icon" href="https://mjones.network/favicon.svg" />
-        <meta property="og:title" content="mjones.network" data-rh="true"/>
-        <meta property="og:description" content="I'm a full-stack software engineer, passionate about online privacy, accessibility, and dark themes. 😎" data-rh="true"/>
-        <meta property="og:image" content="https://mjones.network/avatar.png" data-rh="true"/>
+        <link
+          rel="shortcut icon"
+          href="https://mjones.network/avatar_svg.png"
+        />
+        <link rel="icon" href="https://mjones.network/avatar_svg.png" />
+        <meta property="og:title" content="mjones.network" data-rh="true" />
+        <meta
+          property="og:description"
+          content="I'm a full-stack software engineer, passionate about online privacy, accessibility, and dark themes. 😎"
+          data-rh="true"
+        />
+        <meta
+          property="og:image"
+          content="https://mjones.network/avatar.png"
+          data-rh="true"
+        />
       </Head>
       <div className={classNames.join(" ")}>
         <MainNav useDarkTheme={useDarkTheme} onThemeToggled={toggleDarkTheme} />
