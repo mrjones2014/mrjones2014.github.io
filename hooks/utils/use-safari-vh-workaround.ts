b4c4134b4d5069ac577f276bcafbe295946530eb
Mat Jones <mjones44@ycp.edu>
2021-01-18 17:51:42 -0500

    fix height on mobile safari

--- hooks/utils/use-safari-vh-workaround.ts
+++ hooks/utils/use-safari-vh-workaround.ts
@@ -1,12 +1,10 @@
 import useWindowSize from "hooks/utils/use-window-size";
 import { useEffect } from "react";
-import { BrowserUtils } from "utils/browser-utils";
 
 export default function useSafariVhWorkaround() {
   const { height } = useWindowSize();
 
   useEffect(() => {
-    if (!BrowserUtils.isSafari()) return;
     document.documentElement.style.setProperty("--vh", `${height * 0.01}px`);
   }, [height]);
 }
