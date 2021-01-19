f7791fb7d03ee50b1844a5cf068cbd1eeca2f723
Mat Jones <mjones44@ycp.edu>
2021-01-19 17:30:46 -0500

    fix cross-page scroll issue:

--- components/04-templates/main-layout.tsx
+++ components/04-templates/main-layout.tsx
@@ -2,17 +2,30 @@ import { FocusStyleManager } from "@blueprintjs/core";
 import { MainNav } from "components/02-molecules/main-nav/main-nav";
 import useSafariVhWorkaround from "hooks/utils/use-safari-vh-workaround";
 import { ChildrenProps } from "interfaces/children-props";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Styles from "./main-layout.module.scss";
 import { AppFooter } from "components/02-molecules/app-footer/app-footer";
 import { HeadMeta } from "components/04-templates/head";
+import { useRouter } from "next/router";
 
 export const MainLayout: React.FC<ChildrenProps> = (props: ChildrenProps) => {
+  const router = useRouter();
+
+  const contentRef = useRef<HTMLDivElement>(null);
+
   useSafariVhWorkaround();
 
   useEffect(() => {
     FocusStyleManager.onlyShowFocusOnTabs();
   }, []);
+
+  useEffect(() => {
+    if (contentRef.current == null) {
+      return;
+    }
+
+    contentRef.current.scrollTop = 0;
+  }, [router.pathname]);
 
   const [useDarkTheme, setUseDarkTheme] = useState(true);
 
@@ -31,7 +44,7 @@ export const MainLayout: React.FC<ChildrenProps> = (props: ChildrenProps) => {
       <div className={classNames.join(" ")}>
         <MainNav useDarkTheme={useDarkTheme} onThemeToggled={toggleDarkTheme} />
         <div className={Styles.mainLayout__background} />
-        <div className={Styles.mainLayout__content}>
+        <div className={Styles.mainLayout__content} ref={contentRef}>
           {props.children}
           <AppFooter />
         </div>
