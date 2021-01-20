819074f1ed3eff203f69965551bc3ab1bd4b37cc
Mat Jones <mjones44@ycp.edu>
2021-01-20 17:05:51 -0500

    update config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const DateUtils = require("./utils/date-utils");
+
 const withMDX = require("@next/mdx")({
   extension: /\.(md|mdx)$/,
   options: {
@@ -6,53 +8,13 @@ const withMDX = require("@next/mdx")({
   },
 });
 
-const monthNames = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-
-const getCurrentDateInEST = () => {
-  const offset = -5; // EST timezone offset -5
-  const dateInEST = new Date(new Date().getTime() + offset * 3600 * 1000)
-    .toUTCString()
-    .replace(/ GMT$/, "");
-  return new Date(dateInEST);
-};
-
-const formatTimeOfDay = (date) => {
-  let hours = date.getHours();
-  let minutes = `${date.getMinutes()}`;
-  if (minutes.length < 2) {
-    minutes = `0${minutes}`;
-  }
-  const amOrPm = hours >= 12 ? "PM" : "AM";
-  hours = hours % 12;
-  hours = hours === 0 ? 12 : hours; // 0 should be 12 AM
-  return `${hours}:${minutes} ${amOrPm}`;
-};
-
-const formatCurrentDate = () => {
-  const now = getCurrentDateInEST();
-  console.log(now);
-  return `${
-    monthNames[now.getMonth()]
-  } ${now.getDate()}, ${now.getFullYear()} at ${formatTimeOfDay(now)} EST`;
-};
+const now = DateUtils.getCurrentDateInEST();
+const formattedDate = DateUtils.formatDate(now);
 
 const Config = {
   distDir: "dist",
   env: {
-    NEXT_PUBLIC_LAST_UPDATED_DATE: formatCurrentDate(),
+    NEXT_PUBLIC_LAST_UPDATED_DATE: formattedDate,
   },
   webpack(config) {
     config.module.rules.push({
