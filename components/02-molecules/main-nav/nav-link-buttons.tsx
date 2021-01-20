08dfa3c692555b1261e0b9b48cac35795246641a
Mat Jones <mjones44@ycp.edu>
2021-01-20 17:35:15 -0500

    add phablet breakpoint

--- components/02-molecules/main-nav/nav-link-buttons.tsx
+++ components/02-molecules/main-nav/nav-link-buttons.tsx
@@ -1,12 +1,20 @@
 import { AnchorButton } from "@blueprintjs/core";
+import { NavLinkMenu } from "components/02-molecules/main-nav/nav-link-menu";
 import { NavLink, NavLinks } from "data/nav-links";
+import useWindowSize from "hooks/utils/use-window-size";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
+import { BreakPoints } from "utils/breakpoints";
 import Styles from "./nav-link-buttons.module.scss";
 
 export const NavLinkButtons: React.FC = () => {
   const router = useRouter();
+  const { width } = useWindowSize();
+
+  if (width <= BreakPoints.phablet) {
+    return <NavLinkMenu />;
+  }
 
   return (
     <React.Fragment>
--- utils/breakpoints.ts
+++ utils/breakpoints.ts
@@ -1,6 +1,7 @@
 export const BreakPoints = {
   zero: 0,
   mobile: 320,
+  phablet: 478,
   tablet: 740,
   desktop: 980,
   wide: 1300,
