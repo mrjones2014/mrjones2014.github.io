ef71c980ed9fb57971688814bba42d243974e57f
Mat Jones <mjones44@ycp.edu>
2021-01-21 21:10:19 -0500

    adjust margin

---
+++ pages/how-its-made.tsx
@@ -0,0 +1,11 @@
+import { Card } from "@blueprintjs/core";
+
+// TODO
+/*
+write it as a blog post, then just add some
+custom logic to render that blog post here
+*/
+
+const HowItsMadePage: React.FC = () => <Card></Card>;
+
+// export default HowItsMadePage;
